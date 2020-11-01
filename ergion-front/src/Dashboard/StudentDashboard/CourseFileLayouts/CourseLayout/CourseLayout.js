8babb28d655d6b5c3dac4a0efcf42e1425c24ab2
NeginDerakhshan <shiningneg@gmail.com>
2020-11-01 01:01:22 -0800

    feat:Course widget with material ui

--- ergion-front/src/Dashboard/StudentDashboard/CourseFileLayouts/CourseLayout/CourseLayout.js
+++ ergion-front/src/Dashboard/StudentDashboard/CourseFileLayouts/CourseLayout/CourseLayout.js
@@ -1,83 +1,133 @@
-import React, { Component } from 'react';  
-import Popup from './PopUp';  
-import "./CourseLayout.scss"
-import CourseWidget from './CourseWidget'; 
-import { NextWeek } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
+
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import AddButtonAndPopUp from './PopUp';
+
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
   },
 }));
 
-class App extends Component {  
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  constructor(props){  
-super(props);  
-this.state = { 
-    Course:[],
-    showPopup: false,
-    Adding:false,
-    NextCourse:""
-};  
-}  
+export default function CourseLayout() {
+    const [list, setList] = React.useState([]);
+  const classes = useStyles();
+  const handleCourse=(val)=>
+  {
+    const newList = list.concat({ val });
+ 
+    setList(newList);
+  };
 
-  togglePopup() {  
-this.setState({  
-     showPopup: !this.state.showPopup 
-});  
- }  
- AddCourse() {  
-    this.setState({  
-         showPopup: !this.state.showPopup , 
-         
-         Course:[...this.state.Course,this.state.NextCourse],
-        Adding:true,
-        NextCourse:""
-    });  
-     } 
-     CourseNameHandler=(event)=>
-     {
-        this.setState({NextCourse: event.target.value});
-     }
-  render() {  
-      let Courses=null
-      if(this.state.Adding)
-      {Courses=(
-      <div>{this.state.Course.map(course =>
-      {
-        return(  <CourseWidget name={course} /> );
-      })}
+  return (
+    <React.Fragment>
+      <CssBaseline />
 
-        </div>);
-    
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+
+
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+             
+                  <AddButtonAndPopUp handleNewCourse={handleCourse}/>
+                </Grid>
+               
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {list.map((list) => (
+              <Grid item key={list} xs={12} sm={6} md={4}>
+                <Card className={classes.list}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image='./Coursedemo.png'
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                     Course
+                    </Typography>
+                    <Typography>
+                    more about this Course
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                    مشاهده
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+
+    </React.Fragment>
+  );
 }
-return (  
-<div>  
-
-<button variant="outlined" color="secondary" onClick={this.togglePopup.bind(this)}> Add course</button>  
-
-{this.state.showPopup ?  
-<Popup  
-          text='Add Course'  
-          closePopup={this.togglePopup.bind(this)}  
-          AddThisCourse={this.AddCourse.bind(this)}
-          CourseName={this.CourseNameHandler}
-          CourseNameVal={this.state.NextCourse}
-          open={this.state.showPopup }
-/>  
-: null  
-}  
-{Courses}
-</div>  
-
-
-);  
-}  
-}  
-
-export default App;
--- ergion-front/src/Dashboard/StudentDashboard/CourseFileLayouts/CourseLayout/PopUp.js
+++ ergion-front/src/Dashboard/StudentDashboard/CourseFileLayouts/CourseLayout/PopUp.js
@@ -1,5 +1,5 @@
-import React from 'react';  
 
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,41 +7,63 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-class Popup extends React.Component {  
 
-  render() {  
-return (  
-<div>
+export default function FormDialog(props) {
+  const [open, setOpen] = React.useState(false);
+ 
+  const [name, setName] = React.useState('');
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+ 
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleCloseandAdd = () => {
+   
+    setOpen(false);
+    
+    props.handleNewCourse(name);
 
-<Dialog open={this.props.open} onClose={this.props.closePopup} aria-labelledby="form-dialog-title">
-  <DialogTitle id="form-dialog-title">Add Course</DialogTitle>
-  <DialogContent>
-    <DialogContentText>
-    {this.props.text}
-    </DialogContentText>
-    <TextField
-      autoFocus
-      margin="dense"
-      id="name"
-      label="CourseId"
+ 
+  
+    
+  };
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+      اضافه کردن کلاس جدید
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">کلاس جدید</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          برای اضافه شدن به کلاس شناسه کلاس را وارد نمایید
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="شناسه کلاس"
+            type="Id"
+            fullWidth 
+            value={name} 
+            onChange={handleChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+          لغو
+          </Button>
+          <Button onClick={handleCloseandAdd} color="primary">
+          اضافه شدن
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 
-      fullWidth
-      onChange={this.props.CourseName} 
-      value={this.props.CourseNameVal}
-    />
-  </DialogContent>
-  <DialogActions>
-    <Button onClick={this.props.closePopup} color="primary">
-      Cancel
-    </Button>
-    <Button onClick={this.props.AddThisCourse} color="primary">
-      Subscribe
-    </Button>
-  </DialogActions>
-</Dialog>
-</div>
-);  
-}  
-}  
-
-export default Popup;
