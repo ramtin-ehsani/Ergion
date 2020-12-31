e0ffa042f04e08f285e39c693890716b97ceec7e
ramtin_ehsnai <ramtin.ehsani79@gmail.com>
2020-12-31 21:30:27 +0330

    Fix: clean-ups

--- ergion-front/src/Dashboard/TeacherDashboard/ListItems/ListItems.js
+++ ergion-front/src/Dashboard/TeacherDashboard/ListItems/ListItems.js
@@ -4,7 +4,6 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 import ClassIcon from '@material-ui/icons/Class';
-import SettingsIcon from '@material-ui/icons/Settings';
 import "./ListItems.scss";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Divider from "@material-ui/core/Divider";
